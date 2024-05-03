a0e4695a4332c3fcb8a9a0489c88900e52969cd4
Dima <dimakurinnoy8@gmail.com>
2024-05-04 01:13:32 +0200

    start navbar

---
+++ dashboard_web_wpp/src/app/(pages)/test/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="content">
+      <div className="h-[95%] p-6  absolute overflow-scroll">page</div>
+    </div>
+  );
+};
+
+export default page;
---
+++ dashboard_web_wpp/src/app/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const Navbar = () => {
+  return (
+    <div className=" mx-8  w-[20%] min-h-full ">
+      <div className=" flex flex-col justify-around h-[100%]">
+        <Link href="/" className='flex items-center gap-4'>
+          
+          <Image src="/dashdoard.png" width={50} height={50} alt=""></Image>
+          <h1 className=" text-2xl font-bold">DashboardApp</h1>
+        </Link>
+
+        <nav>
+          <ul className=" text-lg">
+            <li className="mb-7">Dashboard</li>
+            <li className="mb-7">Analytics</li>
+            <li className="mb-7">Order</li>
+            <li className="mb-7">Transactions</li>
+            <li className="mb-7">Shipping</li>
+            <li className="">Product list</li>
+          </ul>
+        </nav>
+        <div className=" text-lg">
+          <ul>
+            <li className="mb-7">Settings</li>
+            <li className="mb-7">Light Mode</li>
+            <li>Logout</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
